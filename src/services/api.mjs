7685f447ca37f5ddcfb1b5b3ab118e2bd04fa150
Osi <osachi752@gmail.com>
2025-05-16 01:21:27 -0400

    integrated user authentication into frontend

--- src/services/api.mjs
+++ src/services/api.mjs
@@ -5,6 +5,24 @@ const API_BASE_URL = 'http://localhost:3000/api'; // backend API base URL
 // Axios instance
 const api = axios.create({
   baseURL: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json', // Default 'Content-Type' for all requests
+  },
 });
+
+// Request interceptor to add the JWT to the Authorization header before sending requests to protected API endpoints
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('authToken'); // retrieve authToken from localStorage
+    // If token exists,
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`; // add the JWT to the Authorization header
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default api;
